5cae1428ea23bec0d2cc8f8a0324f88d1bae84fe
Angel Motta <angel.motta@utec.edu.pe>
2024-10-10 16:36:19 -0500

    feat: GET appointment resource

--- src/test/java/com/angelinux/citasapi/CitasapiApplicationTests.java
+++ src/test/java/com/angelinux/citasapi/CitasapiApplicationTests.java
@@ -1,13 +1,43 @@
 package com.angelinux.citasapi;
 
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class CitasapiApplicationTests {
 
+	@Autowired
+	TestRestTemplate restTemplate;
+
 	@Test
-	void contextLoads() {
+	void shouldReturnAnAppointmentWhenItExists() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/appointments/99", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+		DocumentContext documentContext = JsonPath.parse(response.getBody());
+
+		Number id = documentContext.read("$.id");
+		assertThat(id).isEqualTo(99);
+
+		String dni = documentContext.read("$.dni");
+		assertThat(dni).isEqualTo("42685123");
 	}
 
+
+	@Test
+	void shouldNotReturnAnAppointWithUnknownId() {
+		ResponseEntity<String> response = restTemplate.getForEntity("/api/appointments/1000", String.class);
+
+		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+		assertThat(response.getBody()).isBlank();
+	}
 }
