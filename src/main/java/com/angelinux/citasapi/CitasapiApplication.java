e2b235855839b229c6f8108ffe2ad8aa4632e29f
Angel Motta <angel.motta@utec.edu.pe>
2024-10-09 19:14:12 -0500

    first commit

---
+++ src/main/java/com/angelinux/citasapi/CitasapiApplication.java
@@ -0,0 +1,13 @@
+package com.angelinux.citasapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitasapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitasapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/angelinux/citasapi/CitasapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.angelinux.citasapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CitasapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
