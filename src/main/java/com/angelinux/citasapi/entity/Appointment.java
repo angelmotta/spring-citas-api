4d985b88d52774b01b799e10a3d1dd1193a56173
Angel Motta <angel.motta@utec.edu.pe>
2024-10-12 17:25:42 -0500

    feat: adding HTTP GET Test using testcontainers with postgres

--- src/main/java/com/angelinux/citasapi/entity/Appointment.java
+++ src/main/java/com/angelinux/citasapi/entity/Appointment.java
@@ -17,5 +17,54 @@ public class Appointment {
 
     private Integer specialty;
 
+    public Appointment() {
+    }
 
+    public Appointment(Long id, String firstName, String lastName, String dni, Integer specialty) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.dni = dni;
+        this.specialty = specialty;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public Integer getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(Integer specialty) {
+        this.specialty = specialty;
+    }
 }
--- src/test/java/com/angelinux/citasapi/CitasapiApplicationTests.java
+++ src/test/java/com/angelinux/citasapi/CitasapiApplicationTests.java
@@ -1,13 +1,22 @@
 package com.angelinux.citasapi;
 
+import com.angelinux.citasapi.entity.Appointment;
+import com.angelinux.citasapi.repository.AppointmentRepository;
 import com.jayway.jsonpath.DocumentContext;
 import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -17,16 +26,50 @@ class CitasapiApplicationTests {
 	@Autowired
 	TestRestTemplate restTemplate;
 
+	@Autowired
+	AppointmentRepository appointmentRepository;
+
+	@LocalServerPort
+	private Integer port;
+
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>(
+			"postgres:16"
+	);
+
+	@BeforeAll
+	static void beforeAll() {
+		postgres.start();
+	}
+
+	@AfterAll
+	static void afterAll() {
+		postgres.stop();
+	}
+
+	@DynamicPropertySource
+	static void configureProperties(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
+
+	@BeforeEach
+	void setUp() {
+//		RestAssured.baseURI = "http://localhost:" + port;
+		appointmentRepository.deleteAll();
+	}
+
 	@Test
 	void shouldReturnAnAppointmentWhenItExists() {
-		ResponseEntity<String> response = restTemplate.getForEntity("/api/appointments/99", String.class);
+		var appointmentSaved = appointmentRepository.save(new Appointment(null, "Angel", "Motta", "42685123", 1));
+		Long appointmentId = appointmentSaved.getId();
 
+		ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/appointments/" + appointmentId, String.class);
 		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
 		DocumentContext documentContext = JsonPath.parse(response.getBody());
-
 		Number id = documentContext.read("$.id");
-		assertThat(id).isEqualTo(99);
+		assertThat(id.longValue()).isEqualTo(appointmentId);
 
 		String dni = documentContext.read("$.dni");
 		assertThat(dni).isEqualTo("42685123");
