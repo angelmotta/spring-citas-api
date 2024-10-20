ca747a49d72bd672a6aca6e5df2efd27e0b8a205
Angel Motta <angel.motta@utec.edu.pe>
2024-10-19 20:05:52 -0500

    feat: GET specialty resource implemented

---
+++ src/main/java/com/angelinux/citasapi/specialty/SpecialtyController.java
@@ -0,0 +1,36 @@
+package com.angelinux.citasapi.specialty;
+
+import com.angelinux.citasapi.appointment.domain.AppointmentDTO;
+import com.angelinux.citasapi.specialty.domain.SpecialtyDTO;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/specialties")
+public class SpecialtyController {
+
+    // create static data for test
+    private final List<SpecialtyDTO> specialties = new ArrayList<>(List.of(
+            new SpecialtyDTO(1, "General"),
+            new SpecialtyDTO(2, "Odontología"),
+            new SpecialtyDTO(3, "Pediatría"),
+            new SpecialtyDTO(4, "Psicología")
+    ));
+
+    @GetMapping("/{specialtyId}")
+    public ResponseEntity<SpecialtyDTO> findById(@PathVariable Integer specialtyId) {
+        if (specialtyId < 0 || specialtyId >= specialties.size()) {
+            return ResponseEntity.notFound().build();
+        }
+
+        Optional<SpecialtyDTO> specialityFound = specialties.stream().filter(specialty -> specialty.id().equals(specialtyId)).findFirst();
+        return specialityFound.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
+}
---
+++ src/test/java/com/angelinux/citasapi/SpecialtyApplicationTests.java
@@ -0,0 +1,43 @@
+package com.angelinux.citasapi;
+
+import com.jayway.jsonpath.DocumentContext;
+import com.jayway.jsonpath.JsonPath;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class SpecialtyApplicationTests {
+    @Autowired
+    TestRestTemplate restTemplate;
+
+    @LocalServerPort
+    private Integer port;
+
+    @Test
+    void shouldReturnASpecialtyWhenItExists() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/specialties/2", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        DocumentContext documentContext = JsonPath.parse(response.getBody());
+        Number id = documentContext.read("$.id");
+        assertThat(id.longValue()).isEqualTo(2);
+
+        String specialtyName = documentContext.read("$.specialtyName");
+        assertThat(specialtyName).isEqualTo("Odontología");
+    }
+
+    @Test
+    void shouldNotReturnASpecialtyWithUnknownId() {
+        ResponseEntity<String> response = restTemplate.getForEntity("http://localhost:" + port + "/api/specialties/9999", String.class);
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.NOT_FOUND);
+
+        assertThat(response.getBody()).isBlank();
+    }
+}
